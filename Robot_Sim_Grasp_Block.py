2759e26f77427625fc2f6bc4c9e9e8d2b8ef43b3
jkoneval <112121532+jkoneval@users.noreply.github.com>
2022-12-01 14:23:34 -0600

    Add files via upload

--- Robot_Sim_Grasp_Block.py
+++ Robot_Sim_Grasp_Block.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 14:04:10 2022
+
+@author: jakek
+"""
+
 import pybullet as pb
 import pybullet_data
 import time
@@ -17,53 +24,29 @@ boxes = []
 colorSort = []
 sortPos = []
 sortBoxes = []
-y = .5
-gfinger = .15
-# hard coded color & postion dictionary because we couldn't correctly scale the color_dict from detect_distance
-color_dict = {'1 RED': [[[0.45, y, 0.05], [0.455, 0.169, 0.18, 1]]], '2 ORANGE': [[[0.55, y, 0.05], [0.588, 0.318, 0.2, 1]]], '3 YELLOW': [[[0.15, y, 0.05], [0.541, 0.463, 0.2, 1]]], '4 GREEN': [[[.25, y, 0.05], [0.125, 0.325, 0.216, 1]]], '5 BLUE': [[[0.05, y, 0.05], [0.094, 0.204, 0.325, 1]]]}
+y = 0
+gfinger = .6
 pb.setGravity(0, 0, -9.81)
-numBoxes = 5
+numBoxes = 1
 sortingPosition = []
 boxSortedPosition = []
 # jointPositions=[0.45, 0.458, 0.5, -2.24, -0.30, 0.66, 3]
 jointPositions=[0.5, 0.458, 0.32, -1.7, -0.30, 0.66, 2.32]
 k = 0
 movetime = 20
-griptime = 10
+griptime = 20
 
-for i in range(numBoxes):
-    colorSort.append([])
-    sortPos.append([])
-for i in range(numBoxes):
-    if '1 RED' in color_dict:
-        colorSort[0] = color_dict['1 RED'][0][1]
-        sortPos[0] = color_dict['1 RED'][0][0]
-    if '2 ORANGE' in color_dict:
-        colorSort[1] = color_dict['2 ORANGE'][0][1]
-        sortPos[1] = color_dict['2 ORANGE'][0][0]
-    if '3 YELLOW' in color_dict:
-        colorSort[2] = color_dict['3 YELLOW'][0][1]
-        sortPos[2] = color_dict['3 YELLOW'][0][0]
-    if '4 GREEN' in color_dict:
-        colorSort[3] = color_dict['4 GREEN'][0][1]
-        sortPos[3] = color_dict['4 GREEN'][0][0]
-    if '5 BLUE' in color_dict:
-        colorSort[4] = color_dict['5 BLUE'][0][1]
-        sortPos[4] = color_dict['5 BLUE'][0][0]
+sortPos = [0.6, y, 0.05]
 
-for i in range(numBoxes):
-    endEffectorHeight.append([sortPos[i][0], sortPos[i][1], sortPos[i][2]+.27])
-    endEffectorHeightGrab.append([sortPos[i][0], sortPos[i][1], sortPos[i][2]+.23])
-    sortingPosition.append([.4+.1*i, 0, 0.3])
-    boxSortedPosition.append([.4+.1*i, 0, 0.05])
+endEffectorHeight.append([sortPos[0], sortPos[1], sortPos[2]+.3])
+endEffectorHeightGrab.append([sortPos[0], sortPos[1], sortPos[2]+0.15])
 
 cubeStartOrientation = pb.getQuaternionFromEuler([0,0,0])
 orn=[0.0, 0.0, 1, 0.0]
 flags = pb.URDF_ENABLE_CACHED_GRAPHICS_SHAPES
-for n in colorSort:
-    boxes.append(pb.loadURDF("cube.urdf", sortPos[k], cubeStartOrientation, globalScaling = .1, flags = flags))
-    pb.changeVisualShape(boxes[k], -1, rgbaColor=n)
-    k += 1
+
+boxes.append(pb.loadURDF("cube.urdf", sortPos, cubeStartOrientation, globalScaling = .1, flags = flags))
+pb.changeVisualShape(boxes[k], -1, rgbaColor=[0, 0, 0, 1])
     
 robot = pb.loadSDF('kuka_iiwa/kuka_with_gripper.sdf')
 robot = robot[0]
@@ -80,7 +63,7 @@ planeID = pb.loadURDF("plane.URDF")
 pb.setRealTimeSimulation(0)
 Orientation = pb.getQuaternionFromEuler([np.pi, 0., 0.])
                                            
-time.sleep(10)
+time.sleep(2)
 pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
 targetPositionsJoints = pb.calculateInverseKinematics(robot, numJoints - 7, [0, 0, 3], targetOrientation = Orientation)
 pb.setJointMotorControlArray(robot, range(numJoints-7), pb.POSITION_CONTROL, targetPositions = jointPositions)
@@ -91,7 +74,7 @@ for i in range (0, numBoxes):
     targetPositionsJoints = pb.calculateInverseKinematics(robot, numJoints - 3, endEffectorHeight[i], targetOrientation = Orientation)
     pb.setJointMotorControlArray(robot, range(numJoints-3), pb.POSITION_CONTROL, targetPositions = targetPositionsJoints)
     pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
-    print(targetPositionsJoints)
+    # print(targetPositionsJoints)
     for _ in range(movetime): #move to positions hovering over the blocks
         pb.stepSimulation()
         pb.setJointMotorControl2(robot, 8, pb.POSITION_CONTROL, -.01 ,force= 10)
@@ -126,7 +109,7 @@ for i in range (0, numBoxes):
     targetPositionsJoints = pb.calculateInverseKinematics(robot, numJoints - 3, endEffectorHeight[i], targetOrientation = Orientation)
     pb.setJointMotorControlArray(robot, range(numJoints-3), pb.POSITION_CONTROL, targetPositions = targetPositionsJoints)
     pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
-    print(targetPositionsJoints)
+    # print(targetPositionsJoints)
     for _ in range(movetime): #move to positions hovering over the blocks
             pb.stepSimulation()
             pb.setJointMotorControl2(robot, 8, pb.POSITION_CONTROL, -.01 ,force= 10)
@@ -135,31 +118,20 @@ for i in range (0, numBoxes):
             pb.setJointMotorControl2(robot, 13, pb.POSITION_CONTROL, -.5 ,force= 10)
             pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
             time.sleep(1./10.)
-    targetPositionsJoints = pb.calculateInverseKinematics(robot, numJoints - 3, sortingPosition[i], targetOrientation = Orientation)
-    pb.setJointMotorControlArray(robot, range(numJoints-3), pb.POSITION_CONTROL, targetPositions = targetPositionsJoints)
-    for _ in range(movetime): #move to "sorted" position
-        pb.stepSimulation()
-        pb.setJointMotorControl2(robot, 8, pb.POSITION_CONTROL, -.01 ,force= 100)
-        pb.setJointMotorControl2(robot, 11, pb.POSITION_CONTROL, .01 ,force= 100)
-        pb.setJointMotorControl2(robot, 10, pb.POSITION_CONTROL, .5 ,force= 100)
-        pb.setJointMotorControl2(robot, 13, pb.POSITION_CONTROL, -.5 ,force= 100)
-        pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
-        time.sleep(1./10.)
-    pb.removeBody(boxes[i])
-    sortBoxes.append(pb.loadURDF("cube.urdf", boxSortedPosition[i], cubeStartOrientation, globalScaling = .1, flags = flags))
-    pb.changeVisualShape(boxes[i], -1, rgbaColor=colorSort[i])
-    #make new blocks in the "sorted" positions
-    targetPositionsJoints = pb.calculateInverseKinematics(robot, numJoints - 3, endEffectorHeight[i], targetOrientation = Orientation)
+    targetPositionsJoints = pb.calculateInverseKinematics(robot, numJoints - 3, endEffectorHeightGrab[i], targetOrientation = Orientation)
     pb.setJointMotorControlArray(robot, range(numJoints-3), pb.POSITION_CONTROL, targetPositions = targetPositionsJoints)
     pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
-    for _ in range(movetime): #move to positions hovering over the blocks
-            pb.stepSimulation()
-            pb.setJointMotorControl2(robot, 8, pb.POSITION_CONTROL, -.01 ,force= 10)
-            pb.setJointMotorControl2(robot, 11, pb.POSITION_CONTROL, .01 ,force= 10)
-            pb.setJointMotorControl2(robot, 10, pb.POSITION_CONTROL, .5 ,force= 10)
-            pb.setJointMotorControl2(robot, 13, pb.POSITION_CONTROL, -.5 ,force= 10)
-            pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
-            time.sleep(1./10.)
+    for _ in range(movetime): #move down around the block
+        pb.stepSimulation()
+        time.sleep(1./10.)
+    pb.setJointMotorControl2(robot, 8, pb.POSITION_CONTROL, -gfinger ,force= 100)
+    pb.setJointMotorControl2(robot, 11, pb.POSITION_CONTROL, gfinger ,force= 100)
+    pb.setJointMotorControl2(robot, 10, pb.POSITION_CONTROL, -gfinger ,force= 100)
+    pb.setJointMotorControl2(robot, 13, pb.POSITION_CONTROL, gfinger ,force= 100)
+    pb.setJointMotorControlMultiDof(robot, 7, pb.POSITION_CONTROL)
+    for _ in range(griptime): #open the gripper
+        pb.stepSimulation()
+        time.sleep(1./10.)
 pb.disconnect()
 
 
